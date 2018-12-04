8dfba2c2c744ebfd41cef9c808836a126a8ce031
ma1911-ws025 <ma1911@mail.ru>
2018-12-04 15:20:14 +0300

    задание выполнено

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -1,63 +1,73 @@
 'use strict';
 (function () {
   var draggingElement;
-  //var pinHandle = setup.querySelector('.upload');
-  var firstSetupCoords;
-  //var draggin = false;
+  var elementActived;
+  var currentPosition;
+  var validArea;
 
+  var isValidPisition = function (position) {
+    return (position.x >= validArea.minX && position.x <= validArea.maxX
+      && position.y >= validArea.minY && position.y <= validArea.maxY);
+  };
+
+  var getElementPosition = function () {
+    return {
+      x: Math.floor(parseInt(draggingElement.style.left, 10)),
+      y: Math.floor(parseInt(draggingElement.style.top, 10))
+    };
+  };
 
   var onDraggingElement = function (evt) {
     evt.preventDefault();
 
-    var startCoords = {
+    var startPosition = {
       x: evt.clientX,
       y: evt.clientY,
     };
 
-    //var dragged = false;
-
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
-     // dragged = true;
-      //draggin = true;
       var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY,
+        x: startPosition.x - moveEvt.clientX,
+        y: startPosition.y - moveEvt.clientY,
       };
-      startCoords = {
+      startPosition = {
         x: moveEvt.clientX,
         y: moveEvt.clientY,
       };
 
-      draggingElement.style.top = (draggingElement.offsetTop - shift.y) + 'px';
-      draggingElement.style.left = (draggingElement.offsetLeft - shift.x) + 'px';
+      var tempPosition = {
+        x: draggingElement.offsetLeft - shift.x,
+        y: draggingElement.offsetTop - shift.y
+      };
+
+      if (isValidPisition(tempPosition)) {
+        currentPosition = tempPosition;
+        draggingElement.style.left = currentPosition.x + 'px';
+        draggingElement.style.top = currentPosition.y + 'px';
+      }
     };
 
     var onMouseUp = function () {
-      /*if (dragged) {
-        var onClickPreventDefault = function (inputEvt) {
-          inputEvt.preventDefault();
-          dialogHandle.removeEventListener('click', onClickPreventDefault);
-        };
-        dialogHandle.addEventListener('click', onClickPreventDefault);
-      }*/
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
+      elementActived(currentPosition);
     };
 
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   };
 
-
-  var setDraggin = function (element) {
+  var setDraggin = function (element, area, onElementActive) {
     draggingElement = element;
+    currentPosition = getElementPosition;
+    elementActived = onElementActive;
+    validArea = area;
     draggingElement.addEventListener('mousedown', onDraggingElement);
-
   };
 
   window.dragEndDrop = {
-    setDraggin: setDraggin
+    setDraggin: setDraggin,
+    getElementPosition: getElementPosition
   };
-
 })();
--- js/key-util.js
+++ js/key-util.js
@@ -1,9 +1,7 @@
 'use strict';
 (function () {
-  var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
   window.keyUtil = {
-    ESC_KEYCODE: ESC_KEYCODE,
-    ENTER_KEYCODE: ENTER_KEYCODE
+    ESC_KEYCODE: 27,
+    ENTER_KEYCODE: 13
   };
 })();
